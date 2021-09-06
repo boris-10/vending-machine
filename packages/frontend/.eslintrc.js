f0360a593f50c5dc583337fb79f52b3c45a07909
vlado1010 <vlado+10@codeasy.com>
2021-09-06 10:17:59 +0000

    Setup Eslint and Prettier on both projects
    This will add recommended rules for all packages. Important setting
    is tsconfigRootDir in eslintrc that allows IDEs and CLI tools
    to pick eslint config per package (and not root one).

---
+++ packages/frontend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'no-console': 'warn',
+  },
+}
