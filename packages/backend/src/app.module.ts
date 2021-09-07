1684ed3719be033366b68a33d9ad2d19a0544ddf
vlado1010 <vlado+10@codeasy.com>
2021-09-07 09:41:44 +0000

    Generate products resource
    Generated with Nest CLI

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppConfig } from './app.config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
+import { ProductsModule } from './products/products.module'
 
 @Module({
   imports: [
     UsersModule,
+    ProductsModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [AppConfig],
