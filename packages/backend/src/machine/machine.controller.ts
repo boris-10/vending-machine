7ffb59dd238defe877bc9e161ef6a48a63e3b5e1
vlado1010 <vlado+10@codeasy.com>
2021-09-07 23:44:38 +0000

    Prototype buy, reset and deposit implementations
    Add basic logic with minimum validation.

--- packages/backend/src/machine/machine.controller.ts
+++ packages/backend/src/machine/machine.controller.ts
@@ -1,24 +1,30 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body, UseGuards } from '@nestjs/common'
 import { MachineService } from './machine.service'
 import { BuyRequestDto } from './dto/buy-request.dto'
 import { DepositRequestDto } from './dto/deposit-request.dto'
+import { UserInfo } from '../auth/entity/user-info.entity'
+import { CurrentUser } from '../auth/current-user.decorator'
+import { JwtAuthGuard } from '../auth/jwt-auth.guard'
 
 @Controller('machine')
 export class MachineController {
   constructor(private readonly machineService: MachineService) {}
 
   @Post('deposit')
-  deposit(@Body() { amount }: DepositRequestDto) {
-    return this.machineService.deposit(amount)
+  @UseGuards(JwtAuthGuard)
+  deposit(@Body() { amount }: DepositRequestDto, @CurrentUser() { id: userId }: UserInfo) {
+    return this.machineService.deposit(userId, amount)
   }
 
   @Post('buy')
-  buy(@Body() { productId, amount }: BuyRequestDto) {
-    return this.machineService.buy(productId, amount)
+  @UseGuards(JwtAuthGuard)
+  buy(@Body() { productId, amount }: BuyRequestDto, @CurrentUser() { id: userId }: UserInfo) {
+    return this.machineService.buy(userId, productId, amount)
   }
 
   @Post('reset')
-  reset() {
-    return this.machineService.reset()
+  @UseGuards(JwtAuthGuard)
+  reset(@CurrentUser() { id: userId }: UserInfo) {
+    return this.machineService.reset(userId)
   }
 }
--- packages/backend/src/machine/machine.service.ts
+++ packages/backend/src/machine/machine.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { ProductsService } from 'src/products/products.service'
 import { UsersService } from 'src/users/users.service'
 
@@ -6,15 +6,39 @@ import { UsersService } from 'src/users/users.service'
 export class MachineService {
   constructor(protected readonly usersService: UsersService, protected readonly productsService: ProductsService) {}
 
-  reset() {
-    throw new Error('Method not implemented.')
+  async reset(userId: number) {
+    await this.usersService.update(userId, {
+      deposit: 0,
+    })
   }
 
-  buy(productId: number, amount: number) {
-    throw new Error('Method not implemented.')
+  async buy(userId: number, productId: number, amount: number) {
+    const user = await this.usersService.findOne(userId)
+    const product = await this.productsService.findOne(productId)
+
+    if (amount > product.amountAvailable) {
+      throw new BadRequestException('Not enough items in vending machine. Try reducing buy amount.')
+    }
+
+    const totalPrice = product.cost * amount
+    if (totalPrice > user.deposit) {
+      throw new BadRequestException(`Not enough money to buy: ${amount} ${product.productName}.`)
+    }
+
+    await this.usersService.update(userId, {
+      deposit: user.deposit - totalPrice,
+    })
+
+    await this.productsService.update(userId, {
+      amountAvailable: product.amountAvailable - amount,
+    })
   }
 
-  deposit(amount: number) {
-    throw new Error('Method not implemented.')
+  async deposit(userId: number, amount: number) {
+    const user = await this.usersService.findOne(userId)
+
+    return this.usersService.update(userId, {
+      deposit: user.deposit + amount,
+    })
   }
 }
