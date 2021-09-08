a1df7eea39c726ac14aa35d6c953ea9defc14491
vlado1010 <vlado+10@codeasy.com>
2021-09-08 13:30:41 +0000

    Implement simple mutations for vending machine

--- packages/frontend/src/components/core/VendingMachine.tsx
+++ packages/frontend/src/components/core/VendingMachine.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react'
+import axios from 'axios'
+import { useMutation } from 'react-query'
 
-import Product from '../../models/Product'
 import ProductList from './ProductList'
 import { ProductsContext } from '../../providers/ProductsProvider'
 import Button from '../atoms/Button'
@@ -9,45 +10,71 @@ function VendingMachine(): JSX.Element {
   const [depositedAmount, setDepositedAmount] = useState(0)
   const { selectedProduct, setSelectedProduct } = useContext(ProductsContext)
 
-  const deposit = (amount: number) => {
-    console.log('/deposit API call', amount)
+  const depositMutation = useMutation(async (amount: number) => {
+    await axios.post(
+      'http://localhost:8080/machine/deposit',
+      { amount },
+      {
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxIiwiaWF0IjoxNjMxMTAzMzQ4fQ.yqitnSBq20KnHZybDi8dRHCbIEQ0P8bH4bed37Fu7fQ',
+        },
+      }
+    )
 
-    setDepositedAmount(depositedAmount + amount) // Call when endpoint returns 200
-  }
+    setDepositedAmount(depositedAmount + amount)
+  })
 
-  const reset = () => {
-    console.log('/reset API call')
+  const resetMutation = useMutation(async () => {
+    await axios.post('http://localhost:8080/machine/reset', null, {
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxIiwiaWF0IjoxNjMxMTAzMzQ4fQ.yqitnSBq20KnHZybDi8dRHCbIEQ0P8bH4bed37Fu7fQ',
+      },
+    })
 
-    setDepositedAmount(0) // Call when endpoint returns 200
-  }
+    setDepositedAmount(0)
+  })
 
-  const buyProduct = () => {
-    console.log('/buy API call')
-  }
-
-  const onProductSelect = (product: Product) => {
-    setSelectedProduct(product)
-  }
+  const buyMutation = useMutation(async () => {
+    if (!selectedProduct) {
+      return
+    }
+    await axios.post(
+      'http://localhost:8080/machine/buy',
+      { ...selectedProduct },
+      {
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxIiwiaWF0IjoxNjMxMTAzMzQ4fQ.yqitnSBq20KnHZybDi8dRHCbIEQ0P8bH4bed37Fu7fQ',
+        },
+      }
+    )
+  })
 
   return (
     <div>
-      <ProductList onSelect={onProductSelect} />
+      <ProductList onSelect={(product) => setSelectedProduct(product)} />
       <br />
       Deposit:
       <br />
-      <Button text="5 ¢" onClick={() => deposit(5)} />
-      <Button text="10 ¢" onClick={() => deposit(10)} />
-      <Button text="20 ¢" onClick={() => deposit(20)} />
-      <Button text="50 ¢" onClick={() => deposit(50)} />
-      <Button text="100 ¢" onClick={() => deposit(100)} />
+      <Button text="5 ¢" onClick={() => depositMutation.mutate(5)} />
+      <Button text="10 ¢" onClick={() => depositMutation.mutate(10)} />
+      <Button text="20 ¢" onClick={() => depositMutation.mutate(20)} />
+      <Button text="50 ¢" onClick={() => depositMutation.mutate(50)} />
+      <Button text="100 ¢" onClick={() => depositMutation.mutate(100)} />
       <br />
-      <Button text="Return money" isDisabled={depositedAmount === 0} onClick={() => reset()} />
+      <Button text="Return money" isDisabled={depositedAmount === 0} onClick={() => resetMutation.mutate()} />
       <br />
       Deposited: <b>{depositedAmount}</b> ¢
       <br />
       Selected product: {selectedProduct ? <b>{selectedProduct.productName}</b> : <i>[none]</i>}
       <br />
-      <Button isDisabled={!selectedProduct || depositedAmount < selectedProduct.cost} onClick={buyProduct} text="Buy" />
+      <Button
+        isDisabled={!selectedProduct || depositedAmount < selectedProduct.cost}
+        onClick={() => buyMutation.mutate()}
+        text="Buy"
+      />
     </div>
   )
 }
