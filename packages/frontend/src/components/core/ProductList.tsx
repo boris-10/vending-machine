3cd2e510be3e2912858e3d45a984f08023c452da
vlado1010 <vlado+10@codeasy.com>
2021-09-09 15:25:48 +0000

    Filter products by sellerId

--- packages/frontend/src/components/core/ProductList.tsx
+++ packages/frontend/src/components/core/ProductList.tsx
@@ -2,21 +2,36 @@ import React, { useContext } from 'react'
 import axios from 'axios'
 import { useQuery } from 'react-query'
 
-import Product from '../../models/Product'
 import { ProductsContext } from '../../providers/ProductsProvider'
+import { AuthContext } from '../../providers/AuthProvider'
+import Product from '../../models/Product'
 import ProductItem from './ProductItem'
+import { UserRole } from '../../models/User'
 
 interface ProductListProps {
   onSelect: (product: Product) => void
 }
 
 function ProductList(props: ProductListProps): JSX.Element {
+  const { currentUser } = useContext(AuthContext)
   const { selectedProduct } = useContext(ProductsContext)
-  const { data } = useQuery('fetchProducts', () => axios('/products'))
+
+  const { data } = useQuery(
+    'fetchProducts',
+    async () => {
+      const { data }: { data: Product[] } = await axios('/products')
+
+      if (currentUser?.role === UserRole.Buyer) {
+        return data
+      }
+      return data.filter((product) => product.sellerId === currentUser?.id)
+    },
+    { enabled: !!currentUser }
+  )
 
   return (
     <div className="my-4">
-      {data?.data.map((product: Product) => (
+      {data?.map((product: Product) => (
         <ProductItem
           key={product.id}
           product={product}
