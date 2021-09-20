542f2d8f03260e7af00c587dd94bef8f35a26336
boris-10 <kostres.boris@codeasy.com>
2021-09-20 11:53:55 +0000

    Set Axios interceptor with base URL

--- packages/frontend-next/pages/_app.tsx
+++ packages/frontend-next/pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from 'next/app'
+import axios from 'axios'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 import 'tailwindcss/tailwind.css'
 
 const queryClient = new QueryClient()
+
+axios.interceptors.request.use((request) => {
+  request.baseURL = 'http://localhost:8080'
+
+  const token = localStorage.getItem('jwt')?.toString()
+  if (token) {
+    request.headers['Authorization'] = `Bearer ${token}`
+  }
+  return request
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
