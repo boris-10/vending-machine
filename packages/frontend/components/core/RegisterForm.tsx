50bf61be48d2e3d356024f662b1e4a8e38b7120b
boris-10 <kostres.boris@codeasy.com>
2021-09-23 10:20:06 +0000

    Uppercase role

--- packages/frontend/components/core/RegisterForm.tsx
+++ packages/frontend/components/core/RegisterForm.tsx
@@ -47,7 +47,7 @@ function RegisterForm(props: RegisterFormProps): JSX.Element {
           <div className="rounded-md shadow-sm -space-y-px">
             <Formik
               enableReinitialize
-              initialValues={{ username: '', password: '', role: 'buyer' }}
+              initialValues={{ username: '', password: '', role: 'Buyer' }}
               validate={(model) => {
                 const errors: { username?: string; password?: string } = {}
                 if (!model.username) {
@@ -100,7 +100,7 @@ function RegisterForm(props: RegisterFormProps): JSX.Element {
                 <div className="py-4">
                   <button
                     type="submit"
-                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
                     Register
                   </button>
