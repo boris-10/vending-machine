dd9ee550b4166316ef63bb5ce675a8e9caf9f011
boris-10 <kostres.boris@codeasy.com>
2021-09-23 13:56:37 +0000

    After purchase reset machine

--- packages/frontend/pages/vending-machine.tsx
+++ packages/frontend/pages/vending-machine.tsx
@@ -42,6 +42,8 @@ export default function VendingMachinePage() {
     })
     setCoinChange(coinChange)
     setDepositedAmount(0)
+    setSelectedProduct(null)
+    setSelectedProductAmount(1)
   })
 
   const isPurchaseEnabled = () => {
